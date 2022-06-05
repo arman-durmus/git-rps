627b2e50be3c1013258acd68e5870600dcebddd7
armad00 <armad00@mi.fu-berlin.de>
2022-06-05 15:50:37 +0200

    feat: implement game, call game

--- rps-game.js
+++ rps-game.js
@@ -1,36 +1,63 @@
 function computerPlay(){
-    let results = ["Rock", "Paper", "Scissors"];
+    let results = ["rock", "paper", "scissors"];
     return results[Math.floor(Math.random()*3)];
 }
 
 function playRps(playerSelection, computerSelection){
+    computerSelection = computerSelection.toLowerCase();
     switch(playerSelection){
-        case "Rock":
+        case "rock":
             switch(computerSelection){
-                case "Rock":
-                    return "Draw!"
-                case "Paper":
-                    return "You Lose! Paper beats Rock"
-                case "Scissors":
-                    return "You Win! Rock beats Scissors"
+                case "rock":
+                    return "Draw!";
+                case "paper":
+                    return "You Lose! paper beats rock";
+                case "scissors":
+                    return "You Win! rock beats scissors";
             }
-        case "Paper":
+        case "paper":
             switch(computerSelection){
-                case "Rock":
-                    return "You Win! Paper beats Rock"
-                case "Paper":
-                    return "Draw!"
-                case "Scissors":
-                    return "You Lose! Scissors beat Paper!"
+                case "rock":
+                    return "You Win! paper beats rock";
+                case "paper":
+                    return "Draw!";
+                case "scissors":
+                    return "You Lose! scissors beat paper!";
             }
-        case "Scissors":
+        case "scissors":
             switch(computerSelection){
-                case "Rock":
-                    return "You Lose! Rock beats Scissors"
-                case "Paper":
-                    return "You Win! Scissors beat Paper!"
-                case "Scissors":
-                    return "Draw!"
+                case "rock":
+                    return "You Lose! rock beats scissors";
+                case "paper":
+                    return "You Win! scissors beat paper!";
+                case "scissors":
+                    return "Draw!";
             }
     }
 }
+
+function game(){
+    let winCount = 0;
+    for (let i = 0; i < 5; i++) {
+        input = prompt("Choose rock, paper or scissors!", "rock");
+        res = playRps(input, computerPlay());
+        alert(res);
+        if (res.includes("Win")){
+            winCount += 2;
+        }
+        else if (res == "Draw!"){
+            winCount +=1;
+        }
+     }
+     if (winCount < 5){
+         alert("You Lose the game :(");
+     }
+     else if (winCount == 5){
+         alert("It's a Draw!");
+     }
+     else{
+         alert("Congratulations! You win :)");
+     }
+}
+
+game();
